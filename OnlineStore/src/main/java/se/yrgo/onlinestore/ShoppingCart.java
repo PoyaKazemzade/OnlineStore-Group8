7de4ceb96153313f9697a55d3128322475a9f0e4
Poya Kazemzade <pouya.pua@gmail.com>
2024-02-13 16:47:31 +0100

    implemented shopping cart

--- OnlineStore/src/main/java/se/yrgo/onlinestore/ShoppingCart.java
+++ OnlineStore/src/main/java/se/yrgo/onlinestore/ShoppingCart.java
@@ -1,4 +1,34 @@
 package se.yrgo.onlinestore;
 
-public class ShoppingCart {
+import java.util.ArrayList;
+import java.util.List;
+
+class ShoppingCart {
+    private List<Product> products;
+
+    public ShoppingCart() {
+        this.products = new ArrayList<>();
+    }
+
+    public void addProduct(Product product) {
+        products.add(product);
+    }
+
+    public void removeProduct(Product product) {
+        products.remove(product);
+    }
+
+    public double calculateTotalCost() {
+        double totalCost = 0;
+        for (Product product : products) {
+            totalCost += product.getPrice();
+        }
+        return totalCost;
+    }
+
+    public void viewCartItems() {
+        for (Product product : products) {
+            System.out.println(product.getName());
+        }
+    }
 }
