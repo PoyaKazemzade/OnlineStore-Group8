fc7edd4c6dc1be3de00dc3687dfba78767967e55
adamj0425 <adamj0425@gmail.com>
2024-02-13 17:13:08 +0100

    Implemented the Customer class

--- OnlineStore/src/main/java/se/yrgo/onlinestore/Customer.java
+++ OnlineStore/src/main/java/se/yrgo/onlinestore/Customer.java
@@ -1,25 +1,76 @@
 package se.yrgo.onlinestore;
 
+
 import java.util.List;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 
 public class Customer {
     private int customerId;
     private String name;
     private String email;
 
+    private List<ShoppingCart> orderHistory;
+
+
     public Customer(int customerId, String name, String email) {
         this.customerId = customerId;
         this.name = name;
         this.email = email;
+        this.orderHistory = new ArrayList<>();
+
     }
 
     public void viewOrderHistory() {
+        for (ShoppingCart shoppingcart : orderHistory) {
+            System.out.println();
+        }
+    }
 
+    public void placeOrder(ShoppingCart shoppingCart) {
+        orderHistory.add(shoppingCart);
     }
 
 
-    public void placeOrder() {
+    public int getCustomerId() {
+        return customerId;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public List<ShoppingCart> getOrderHistory() {
+        return orderHistory;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer:" + '\''+
+                "CustomerId: " + customerId + '\''+
+                "Name: " + name + '\'' +
+                "Email: " + email + '\'' +
+                "OrderHistory: " + orderHistory;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Customer customer = (Customer) o;
+        return customerId == customer.customerId && Objects.equals(name, customer.name) && Objects.equals(email, customer.email) && Objects.equals(orderHistory, customer.orderHistory);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(customerId, name, email, orderHistory);
     }
 }
 
--- OnlineStore/src/main/java/se/yrgo/onlinestore/Product.java
+++ OnlineStore/src/main/java/se/yrgo/onlinestore/Product.java
@@ -1,5 +1,7 @@
 package se.yrgo.onlinestore;
 
+import java.util.Objects;
+
 public class Product {
     private int productId;
     private String name;
@@ -47,6 +49,29 @@ public class Product {
 
     public int getQuantity() {
         return quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "productId=" + productId +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", quantity=" + quantity +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return productId == product.productId && Double.compare(price, product.price) == 0 && quantity == product.quantity && Objects.equals(name, product.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(productId, name, price, quantity);
     }
 }
 
